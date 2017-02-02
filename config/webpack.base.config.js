d0f2ece3bb5563e1901991a4df76d182684a272e
H5DevHoward <h5devhoward@gmail.com>
2017-02-02 14:03:42 +0800

    initial commit

---
+++ config/webpack.base.config.js
@@ -0,0 +1,43 @@
+const webpack = require('webpack');
+const postcssConfig = require('./postcss.config');
+
+module.exports = {
+    entry: './dev/script/index.js',
+    output: {
+        path: './dist',
+        publicPath: 'dist/',
+        filename: 'build.js',
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.js',
+        },
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue',
+                options: {
+                    postcss: postcssConfig.plugins,
+                },
+            },
+            {
+                test: /\.s[a|c]ss$/,
+                loader: 'style!css?sourceMap!postcss!sass',
+            },
+        ],
+    },
+    postcss: postcssConfig,
+    vue: {
+        loaders: {
+            js: 'babel',
+            scss: 'style!css!sass',
+        },
+    },
+};
