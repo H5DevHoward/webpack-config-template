63a02082d80cacd60431ffc1d7b7193d2238944a
H5DevHoward <h5devhoward@gmail.com>
2017-05-26 09:12:03 +0800

    u

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -53,14 +53,14 @@ module.exports = {
                 test: /\.css$/,
                 use: [
                     'style-loader',
-                    'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
+                    'css-loader',
                 ],
             },
             {
                 test: /\.s[a|c]ss$/,
                 use: [
                     'style-loader',
-                    'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
+                    'css-loader',
                     {
                         loader: 'postcss-loader',
                         options: postcssConfig,
