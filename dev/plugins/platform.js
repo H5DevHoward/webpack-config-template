cd935baeca3d988ad17e532d26650a86c0c5ccb5
H5DevHoward <h5devhoward@gmail.com>
2017-07-05 14:37:46 +0800

    update eslintignore config

--- dev/plugins/platform.js
+++ dev/plugins/platform.js
@@ -9,22 +9,22 @@
         // Browser global
         factory();
     }
-}(this, function() {
-    //////////
+}(this, () => {
+    // ////////
     // Name //
-    //////////
+    // ////////
 
-    var COMPONENT_NAME = 'Platform';
+    const COMPONENT_NAME = 'Platform';
 
-    ///////////////
+    // /////////////
     // Component //
-    ///////////////
+    // /////////////
 
-    var ua = window.navigator.userAgent.toLowerCase();
-    var html = document.getElementsByTagName('html')[0];
+    const ua = window.navigator.userAgent.toLowerCase();
+    const html = document.getElementsByTagName('html')[0];
 
-    var Component = {
-        hasTouch: ('ontouchstart' in window),
+    const Component = {
+        hasTouch: 'ontouchstart' in window,
         isiPad: ua.match(/ipad/i) !== null,
         isNexus7: ua.match(/Nexus 7/gi) !== null,
         isMobile: ua.match(/Android|webOS|iPhone|iPod|BlackBerry|IEMobile/i) !== null && ua.match(/Mobile/i) !== null,
@@ -33,7 +33,7 @@
         isS4: ua.match(/(gt\-i95)|(sph\-l720)/i) !== null,
         isS5: ua.match(/sm\-g900/i) !== null,
         isS6: ua.match(/sm\-g9250/i) !== null,
-        isS7: (ua.match(/sm\-g930p/i) !== null) || (ua.match(/sm\-g9300/i) !== null),
+        isS7: ua.match(/sm\-g930p/i) !== null || ua.match(/sm\-g9300/i) !== null,
         isIE: ua.match(/(msie|trident)/i) !== null,
         isIE11: ua.match(/Trident\/7\.0/i) !== null,
         isEdge: ua.match(/edge/i) !== null,
@@ -51,9 +51,8 @@
     Component.isIOS = Component.isiPad || Component.isiPhone;
 
 
-
-    Object.keys(Component).forEach(function(key) {
-        var className = key.toLowerCase().replace('is', '');
+    Object.keys(Component).forEach(key => {
+        let className = key.toLowerCase().replace('is', '');
 
         if (className.indexOf('has') === 0) {
             className = className.replace('has', 'has-');
@@ -63,7 +62,7 @@
             if (className.indexOf('has') === 0) {
                 className = className.replace('has', 'no');
             } else {
-                className = 'not-' + className;
+                className = `not-${className}`;
             }
         }
 
